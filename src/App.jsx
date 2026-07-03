531b78b6608155cb54133b6abb818b82346cdbc9
namyoon0418 <namyoon0418@gmail.com>
2026-07-04 00:30:13 +0900

    feat: images폴더 생성, ProgressBar 컴포넌트 생성, 회원가입 페이지 생성 (#5)
    * feat: Login페이지 생성, Fonts.css 파일 생성
    
    * feat: images폴더 생성, ProgressBar 컴포넌트 생성, 회원가입 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from './styles/AppLayout';
 import Login from './pages/Login.jsx';
+import Signup from './pages/Signup.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <AppLayout>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
         </Routes>
       </AppLayout>
     </BrowserRouter>
