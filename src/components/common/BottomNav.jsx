cd631064e5646e0d4cdbee8a5d161aff2f5fe13f
Lim Sanghyun <sh2624@daum.net>
2026-07-06 15:02:34 +0900

    feat: 홈 화면 구현, 인트로 수정 (#8)

---
+++ src/components/common/BottomNav.jsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import homeIcon from '../../assets/icons/HomeIcon.svg';
+import campIcon from '../../assets/icons/CampIcon.svg';
+import searchIcon from '../../assets/icons/SearchIcon.svg';
+import mypageIcon from '../../assets/icons/MypageIcon.svg';
+
+export default function BottomNav() {
+  const navigate = useNavigate();
+
+  return (
+    <NavContainer>
+      <NavItem onClick={() => navigate('/')}>
+        <Icon src={homeIcon} />
+        <NavText className="active">홈</NavText>
+      </NavItem>
+
+      <NavItem onClick={() => navigate('/basecamp')}>
+        <Icon src={campIcon} />
+        <NavText>베이스캠프</NavText>
+      </NavItem>
+
+      <NavItem onClick={() => navigate('/explore')}>
+        <Icon src={searchIcon} />
+        <NavText>탐색</NavText>
+      </NavItem>
+
+      <NavItem onClick={() => navigate('/mypage')}>
+        <Icon src={mypageIcon} />
+        <NavText>마이페이지</NavText>
+      </NavItem>
+    </NavContainer>
+  );
+}
+
+const NavContainer = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: #ffffff;
+  border-top: 2px solid #eeeeee;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const NavItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  padding: 10px;
+`;
+
+const Icon = styled.img`
+  width: 36px;
+  height: auto;
+`;
+
+const NavText = styled.span`
+  font-size: 14px;
+  color: #888888;
+  font-weight: 500;
+
+  &.active {
+    color: #5d986a;
+    font-weight: 700;
+  }
+`;
