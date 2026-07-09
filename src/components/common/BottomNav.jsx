ad1caf14e64c843f791729b5d45c5fb7cfe33d4d
sh2624 <sh2624@daum.net>
2026-07-09 23:25:57 +0900

    style: 탐색, 하단바, 마이페이지 UI 수정

--- src/components/common/BottomNav.jsx
+++ src/components/common/BottomNav.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import homeIcon from '../../assets/icons/HomeIcon.svg';
 import campIcon from '../../assets/icons/CampIcon.svg';
 import searchIcon from '../../assets/icons/SearchIcon.svg';
@@ -7,27 +7,48 @@ import mypageIcon from '../../assets/icons/MypageIcon.svg';
 
 export default function BottomNav() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const checkIsActive = (path) => location.pathname === path; // 경로 확인 함수
 
   return (
     <NavContainer>
       <NavItem onClick={() => navigate('/home')}>
-        <Icon src={homeIcon} />
-        <NavText className="active">홈</NavText>
+        <Icon
+          src={homeIcon}
+          className={checkIsActive('/home') ? 'active' : ''}
+        />
+        <NavText className={checkIsActive('/home') ? 'active' : ''}>홈</NavText>
       </NavItem>
 
       <NavItem onClick={() => navigate('/basecamp')}>
-        <Icon src={campIcon} />
-        <NavText>베이스캠프</NavText>
+        <Icon
+          src={campIcon}
+          className={checkIsActive('/basecamp') ? 'active' : ''}
+        />
+        <NavText className={checkIsActive('/basecamp') ? 'active' : ''}>
+          베이스캠프
+        </NavText>
       </NavItem>
 
       <NavItem onClick={() => navigate('/explore')}>
-        <Icon src={searchIcon} />
-        <NavText>탐색</NavText>
+        <Icon
+          src={searchIcon}
+          className={checkIsActive('/explore') ? 'active' : ''}
+        />
+        <NavText className={checkIsActive('/explore') ? 'active' : ''}>
+          탐색
+        </NavText>
       </NavItem>
 
       <NavItem onClick={() => navigate('/mypage')}>
-        <Icon src={mypageIcon} />
-        <NavText>마이페이지</NavText>
+        <Icon
+          src={mypageIcon}
+          className={checkIsActive('/mypage') ? 'active' : ''}
+        />
+        <NavText className={checkIsActive('/mypage') ? 'active' : ''}>
+          마이페이지
+        </NavText>
       </NavItem>
     </NavContainer>
   );
@@ -35,9 +56,9 @@ export default function BottomNav() {
 
 const NavContainer = styled.div`
   width: 100%;
-  height: 60px;
+  height: 68px;
   background-color: #ffffff;
-  border-top: 2px solid #eeeeee;
+  border-top: 1px solid #eeeeee;
   display: flex;
   align-items: center;
   justify-content: space-around;
@@ -55,6 +76,13 @@ const NavItem = styled.div`
 const Icon = styled.img`
   width: 36px;
   height: auto;
+
+  filter: grayscale(100%) opacity(40%);
+  transition: all 0.2s ease-in-out;
+
+  &.active {
+    filter: none;
+  }
 `;
 
 const NavText = styled.span`
